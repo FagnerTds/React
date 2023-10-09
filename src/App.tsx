dac02aaaa37ea6b478c7581cb8a4665e2d0d0244
Fagner Tranquilino <fagnertds@gmail.com>
2023-10-09 11:37:50 -0300

    Criação da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Home from './assets/paginas/home/Home'
-
-const App = () => {
+import React from 'react';
+import './App.css';
+import Home from './assets/paginas/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './assets/paginas/login/Login';
+function App() {
   return (
     <>
-    <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
